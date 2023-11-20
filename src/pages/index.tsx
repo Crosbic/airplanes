6255ef829bc49e435d117d5b8aaec60d588022d9
Crosbic <thecrosbic@gmail.com>
2023-11-20 17:46:24 +0300

    validation + toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,76 @@
 import ArrowDownwardOutlinedIcon from '@mui/icons-material/ArrowDownwardOutlined'
 import ArrowUpwardOutlinedIcon from '@mui/icons-material/ArrowUpwardOutlined'
 import {
+  Alert,
   Box,
   Button,
   FormControlLabel,
-  InputBase,
+  Snackbar,
   styled,
   Switch,
+  TextField,
   Typography,
 } from '@mui/material'
-import { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 import styles from '@/styles/Home.module.css'
 
 export default function Home() {
-  const [isVisibleTakeOff, setIsVisibleTakeOff] = useState<boolean>(false)
-  const [isVisibleLanding, setisVisibleLanding] = useState<boolean>(false)
-  const [disable, setDisable] = useState({
+  const [open, setOpen] = useState(false)
+  const [isVisibleTakeOff, setIsVisibleTakeOff] = useState<boolean>(true)
+  const [isVisibleLanding, setisVisibleLanding] = useState<boolean>(true)
+  const [switchState, setSwitchState] = useState({
     takeoff: false,
     landing: false,
   })
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setDisable({
-      ...disable,
+  const handleSwitchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSwitchState({
+      ...switchState,
       [event.target.name]: event.target.checked,
     })
   }
 
-  const StyledInput = styled(InputBase)(({ theme }) => ({
-    'label + &': {
-      marginTop: theme.spacing(3),
+  const handleClick = () => {
+    setOpen(true)
+  }
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    setOpen(false)
+  }
+
+  const StyledInput = styled(TextField)({
+    '& .MuiOutlinedInput-input': {
+      padding: '0.4rem',
     },
-    '& .MuiInputBase-input': {
-      position: 'relative',
-      backgroundColor: 'transparent',
-      border: '1px solid',
+    '& input:valid + fieldset': {
       borderColor: '#030092',
-      fontSize: 16,
-      padding: '0 0.5rem',
-      width: 'auto',
+      borderWidth: 2,
     },
-  }))
+    '& input:valid:focus + fieldset': {
+      borderColor: '#030092',
+      borderWidth: 2,
+    },
+    '& input:invalid + fieldset': {
+      borderColor: 'red',
+      borderWidth: 2,
+    },
+    '& input:invalid:hover + fieldset': {
+      borderColor: '#030092',
+      borderWidth: 2,
+    },
+    '& input:invalid:focus + fieldset': {
+      borderColor: '#030092',
+      borderWidth: 2,
+    },
+  })
 
   return (
     <>
@@ -59,26 +88,33 @@ export default function Home() {
                     <Typography sx={{ paddingBottom: '0.2rem' }}>
                       S (wing area):
                     </Typography>
-                    <StyledInput placeholder="100m" />
+                    <StyledInput
+                      className={styles.input}
+                      required
+                      type="number"
+                      placeholder="100m"
+                    />
                   </div>
                   <div style={{ paddingBottom: '1rem' }}>
                     <Typography sx={{ paddingBottom: '0.2rem' }}>
                       M (airplane weight):
                     </Typography>
-                    <StyledInput placeholder="100m" />
+                    <StyledInput required type="number" placeholder="100m" />
                   </div>
-                  <Typography sx={{ paddingBottom: '0.2rem' }}>
-                    P (thrust):
-                  </Typography>
-                  <StyledInput placeholder="100m" />
+                  <div>
+                    <Typography sx={{ paddingBottom: '0.2rem' }}>
+                      P (thrust):
+                    </Typography>
+                    <StyledInput required type="number" placeholder="100m" />
+                  </div>
                 </div>
                 <div className={styles.take_off}>
                   <div className={styles.header_switch_arrow}>
                     <FormControlLabel
                       control={
                         <Switch
-                          checked={disable.takeoff}
-                          onChange={handleChange}
+                          checked={switchState.takeoff}
+                          onChange={handleSwitchChange}
                           size="small"
                           name="takeoff"
                         />
@@ -107,18 +143,25 @@ export default function Home() {
                         <Typography sx={{ paddingBottom: '0.2rem' }}>
                           Cy take off:
                         </Typography>
-                        <StyledInput placeholder="1.033 " />
+                        <StyledInput
+                          required
+                          type="number"
+                          placeholder="1.033 "
+                        />
                       </div>
                       <div style={{ paddingBottom: '1rem' }}>
                         <Typography sx={{ paddingBottom: '0.2rem' }}>
                           Airport(name/IATA/ICAO):
                         </Typography>
-                        <StyledInput placeholder="Name" />
+                        <StyledInput required placeholder="Name" />
                       </div>
                       <Typography sx={{ paddingBottom: '0.2rem' }}>
                         Airport coordinates(lon; lat):
                       </Typography>
-                      <StyledInput placeholder="55.755831; 37,617673" />
+                      <StyledInput
+                        required
+                        placeholder="55.755831; 37,617673"
+                      />
                     </div>
                   ) : (
                     <></>
@@ -130,8 +173,8 @@ export default function Home() {
                   <FormControlLabel
                     control={
                       <Switch
-                        checked={disable.landing}
-                        onChange={handleChange}
+                        checked={switchState.landing}
+                        onChange={handleSwitchChange}
                         size="small"
                         name="landing"
                       />
@@ -143,7 +186,6 @@ export default function Home() {
                       className={styles.arrow}
                       onClick={() => {
                         setisVisibleLanding(false)
-                        console.log('closed')
                       }}
                     />
                   ) : (
@@ -151,7 +193,6 @@ export default function Home() {
                       className={styles.arrow}
                       onClick={() => {
                         setisVisibleLanding(true)
-                        console.log('opened')
                       }}
                     />
                   )}
@@ -160,9 +201,13 @@ export default function Home() {
                   <div>
                     <div style={{ paddingBottom: '1rem' }}>
                       <Typography sx={{ paddingBottom: '0.2rem' }}>
-                        Cy take off:
+                        Cy land:
                       </Typography>
-                      <StyledInput placeholder="1.033 " />
+                      <StyledInput
+                        required
+                        type="number"
+                        placeholder="1.033 "
+                      />
                     </div>
                     <div
                       style={{
@@ -176,13 +221,16 @@ export default function Home() {
                         <Typography sx={{ paddingBottom: '0.2rem' }}>
                           Airport(name/IATA/ICAO):
                         </Typography>
-                        <StyledInput placeholder="Name" />
+                        <StyledInput required placeholder="Name" />
                       </div>
                       <div>
                         <Typography sx={{ paddingBottom: '0.2rem' }}>
                           Airport coordinates(lon; lat):
                         </Typography>
-                        <StyledInput placeholder="55.755831; 37,617673" />
+                        <StyledInput
+                          required
+                          placeholder="55.755831; 37,617673"
+                        />
                       </div>
                     </div>
                   </div>
@@ -193,8 +241,9 @@ export default function Home() {
               <Button
                 variant="contained"
                 color="secondary"
-                disabled={!disable.takeoff || !disable.landing}
+                disabled={!switchState.takeoff || !switchState.landing}
                 sx={{ margin: '2rem 1rem 0 1rem', padding: '0.3rem 2rem' }}
+                onClick={handleClick}
               >
                 Send request
               </Button>
@@ -235,6 +284,17 @@ export default function Home() {
           </div>
         </Box>
       </div>
+      <>
+        <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            sx={{ width: '100%' }}
+          >
+            Successfully!
+          </Alert>
+        </Snackbar>
+      </>
     </>
   )
 }
